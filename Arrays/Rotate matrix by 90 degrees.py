a7babfa22bf0c40d97db924bd5134479320c9a81
Venkata Abhishek <venkataabhishek054@gmail.com>
2026-04-23 00:21:11 -0500

    Add matrix rotation function
    Implement function to rotate a matrix by 90 degrees.

---
+++ Arrays/Rotate matrix by 90 degrees.py
@@ -0,0 +1,11 @@
+def rotateMatrix(self, matrix):
+    n = len(matrix)
+    for i in range(n):
+        for j in range(i):
+            matrix[i][j], matrix[j][i]=matrix[j][i], matrix[i][j]
+    
+    for i in range(n):
+        for j in range(n//2):
+            matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+    
+    return matrix
