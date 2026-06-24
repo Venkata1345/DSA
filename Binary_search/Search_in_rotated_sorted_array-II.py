a7a0b101465e61f4a01c93cb3a0636444c08d0df
Venkata Abhishek <venkataabhishek054@gmail.com>
2026-06-24 18:43:31 -0500

    Implement search in rotated sorted array II

---
+++ Binary_search/Search_in_rotated_sorted_array-II.py
@@ -0,0 +1,24 @@
+class Solution:
+    def searchInARotatedSortedArrayII(self, nums, k):
+        low,high = 0,len(nums)-1
+        while low<=high:
+            mid = (low+high)//2
+            if nums[mid]==k:
+                return True
+            if nums[low]==nums[mid]==nums[high]:
+                low+=1
+                high-=1
+                continue
+            if nums[low]<=nums[mid]:
+                if nums[low]<=k<=nums[mid]:
+                    high = mid-1
+                else:
+                    low = mid+1
+            else:
+                if nums[mid]<=k<=nums[high]:
+                    low = mid+1
+                else:
+                    high = mid-1
+            
+            
+        return False
