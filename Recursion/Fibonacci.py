7d97f85c4818fe763761a41e39b1abdde95061cb
Venkata Abhishek <venkataabhishek054@gmail.com>
2026-08-04 10:41:44 -0500

    Implement Fibonacci function using recursion

---
+++ Recursion/Fibonacci.py
@@ -0,0 +1,8 @@
+class Solution:
+    def fib(self, n):
+        #your code goes here
+        if n==0:
+            return 0
+        if n==1:
+            return 1
+        return self.fib(n-1)+self.fib(n-2)
